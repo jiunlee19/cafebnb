59c975df23910ac0d8d89e321b1e83db29cf14e7
jiunlee19 <tearofglass@gachon.ac.kr>
2025-05-19 10:40:47 +0900

    데이터 바인딩 끝

--- JS/CafeList.js
+++ JS/CafeList.js
@@ -4,20 +4,23 @@ fetch('cafes.json')
     window.cafeData = data;
     initSlider(data)
     
-    const container = document.getElementById('cafe-list');
+    const container = document.querySelectorAll('.cafe-list');
 
-    data.forEach(cafe => {
-      const cafeDiv = document.createElement('div');
-      cafeDiv.classList.add('cafe');
-
-      cafeDiv.innerHTML = `
-        <img src="${cafe.image}" alt="${cafe.name}">
-        <h2>${cafe.name}</h2> 
-        <p>${cafe.location}</p>
-        <p>${cafe.score}</p>
-      `;
-
-      container.appendChild(cafeDiv);
-    });
+    container.forEach(container => {
+      data.forEach(cafe => {
+        const cafeDiv = document.createElement('div');
+        cafeDiv.classList.add('cafe');
+  
+        cafeDiv.innerHTML = `
+          <img src="${cafe.image}" alt="${cafe.name}">
+          <h2>${cafe.name}</h2> 
+          <p>${cafe.location}</p>
+          <p>${cafe.score}</p>
+        `;
+  
+        container.appendChild(cafeDiv);
+      });
+    })
+    
   })
   .catch(error => console.error('Error loading cafes:', error));
--- JS/SlideButtons.js
+++ JS/SlideButtons.js
@@ -1,13 +1,17 @@
-const imageSlide = document.querySelector('#cafe-list');
+const imageSlide = document.querySelector('.cafe-list');
         const prevBtn = document.querySelector('#prev-btn');
         const nextBtn = document.querySelector('#next-btn');
     
         let position = 0;              // 현재 위치
-        const step = 12;              // 이동 단위 rem
+        const step = 25;              // 이동 단위 rem
         let maxSteps = 0;
         function initSlider(cafeData) {
-            maxSteps = cafeData.length;
-          }          // 최대 이동 횟수
+            maxSteps = cafeData.length - 4;// 최대 이동 횟수
+
+            // 초기 상태 설정
+            updateButtonsColor();
+          }          
+          
     
         function updateButtonsColor() {
             // prev 버튼 색상 설정
@@ -34,7 +38,7 @@ const imageSlide = document.querySelector('#cafe-list');
         prevBtn.addEventListener('click', function () {
             if (position < 0) {
                 position += step; // 오른쪽으로 이동
-                imageSlide.style.transform = `translateX(${position}rem)`;
+                imageSlide.style.transform = `translateX(${position}vw)`;
             }
             updateButtonsColor();
         });
@@ -42,10 +46,7 @@ const imageSlide = document.querySelector('#cafe-list');
         nextBtn.addEventListener('click', function () {
             if (position > -step * maxSteps) {
                 position -= step; // 왼쪽으로 이동
-                imageSlide.style.transform = `translateX(${position}rem)`;
+                imageSlide.style.transform = `translateX(${position}vw)`;
             }
             updateButtonsColor();
         });
-    
-        // 초기 상태 설정
-        updateButtonsColor();
