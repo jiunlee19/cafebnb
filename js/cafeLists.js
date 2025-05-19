f3026ac7655b83757d43500e966349b89db96285
jiunlee19 <tearofglass@gachon.ac.kr>
2025-05-19 18:22:21 +0900

    link 경로 수정

--- js/cafeLists.js
+++ js/cafeLists.js
@@ -10,7 +10,7 @@ fetch('cafes.json')
       data.forEach(cafe => {
         const cafeDiv = document.createElement('a');
         cafeDiv.classList.add('cafe');
-        cafeDiv.href = '../detail.html';
+        cafeDiv.href = 'cafebnb/detail.html';
         cafeDiv.innerHTML = `
           <img src="${cafe.image}" alt="${cafe.name}">
           <h2>${cafe.name}</h2> 
